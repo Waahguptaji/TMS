2ceb1bc70e5de688267aa4fa4b66eb94e8c650a9
waahguptaji <sahilgupta742@gmail.com>
2024-12-05 22:54:01 +0530

    Revamping code

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { BrowserModule } from '@angular/platform-browser';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserModule)],
-};
